c55bd6d67b298ab3f9dcc8e2ef65cf302d7ead63
Sushma <98072240+Sushma0204@users.noreply.github.com>
2023-10-21 10:53:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import React from 'react';
+
+import UserList from './components/UserList';
+
+
+function App() {
+
+  return (
+    <>
+     
+      {/* REACT TASK-4 */}
+      <div className="App">
+        <UserList />
+      </div>
+    </>
+
+  );
+}
+
+export default App;
